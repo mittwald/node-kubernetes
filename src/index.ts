e20ab397d22752f03e68d29643aabafe282912f5
Martin Helmich <m.helmich@mittwald.de>
2017-05-11 12:55:28 +0200

    Add missing index.ts

---
+++ src/index.ts
@@ -0,0 +1,4 @@
+export * from "./types";
+export * from "./client";
+export * from "./config";
+export * from "./resource";
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,3 +1,4 @@
 export * from "./config";
 export * from "./meta";
 export * from "./pod";
+export * from "./persistentvolume";

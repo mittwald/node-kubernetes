76fc3cdf92daef6383f9984e60e5e3be77315da3
Martin Helmich <m.helmich@mittwald.de>
2017-06-08 17:00:30 +0200

    Adjust Deployment+StatefulSet clients

--- src/resource/statefulset.ts
+++ src/resource/statefulset.ts
@@ -2,14 +2,7 @@ import {NamespacedResourceClient} from "../resource";
 import {IKubernetesRESTClient} from "../client";
 import {LabelSelector} from "../label";
 import {StatefulSet} from "../types/statefulset";
-
-export interface IStatefulSetDeletionOptions {
-    orphanDependents: boolean;
-}
-
-const defaultStatefulSetDeletionOptions: IStatefulSetDeletionOptions = {
-    orphanDependents: false,
-};
+import {DeleteOptions} from "../types/meta";
 
 export class StatefulSetResourceClient extends NamespacedResourceClient<StatefulSet, "StatefulSet", "apps/v1beta1"> {
 
@@ -25,23 +18,8 @@ export class StatefulSetResourceClient extends NamespacedResourceClient<Stateful
         return super.allNamespaces() as StatefulSetResourceClient;
     }
 
-    public async delete(resourceOrName: string|StatefulSet,
-                        opts: Partial<IStatefulSetDeletionOptions> = {}): Promise<void> {
-        let url;
-
-        opts = {...defaultStatefulSetDeletionOptions, ...opts};
-
-        if (typeof resourceOrName === "string") {
-            url = this.baseURL + "/" + resourceOrName;
-        } else {
-            url = this.urlForResource(resourceOrName);
-        }
-
-        return await this.client.delete(url, undefined, {orphanDependents: opts.orphanDependents ? "true" : "false"});
-    }
-
     public async deleteMany(labelSelector: LabelSelector,
-                            opts: Partial<IStatefulSetDeletionOptions> = {}) {
+                            opts: DeleteOptions = {}) {
         const resources = await this.list(labelSelector);
         await Promise.all(resources.map(r => this.delete(r, opts)));
     }

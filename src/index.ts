d0db10d0d2b7bc4811a8f761f67c367388d6d3d7
Martin Helmich <m.helmich@mittwald.de>
2018-05-04 11:50:17 +0200

    Update index file

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,6 @@ export * from "./api";
 export * from "./apis";
 export * from "./client";
 export * from "./client_ratelimit";
+export * from "./client_monitoring";
 export * from "./config";
 export * from "./resource";

c561f28604225c9d78f724258acfdcccd311b2ee
Martin Helmich <m.helmich@mittwald.de>
2018-05-15 08:37:05 +0200

    Add support for PodDisruptionBudgets

--- src/apis/index.ts
+++ src/apis/index.ts
@@ -3,3 +3,4 @@ export * from "./batch";
 export * from "./core";
 export * from "./extensions";
 export * from "./rbac";
+export * from "./policy";
---
+++ src/types/policy/v1beta1/poddisruptionbudget.ts
@@ -0,0 +1,14 @@
+import {LabelSelector, ObjectMeta} from "../../meta/v1";
+
+export interface PodDisruptionBudget {
+    metadata: ObjectMeta;
+    spec: PodDisruptionBudgetSpec;
+}
+
+export type PodDisruptionBudgetSpec = {
+    selector: LabelSelector;
+} & ({
+    maxUnavailable: number|string;
+} | {
+    minAvailable: number|string;
+});

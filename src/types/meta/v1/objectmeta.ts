8138c78fca53094ec7ba5809972e65191f82657d
Martin Helmich <m.helmich@mittwald.de>
2018-12-06 10:29:10 +0100

    Add "finalizers" meta data property

--- src/types/meta/v1/objectmeta.ts
+++ src/types/meta/v1/objectmeta.ts
@@ -4,4 +4,7 @@ export interface ObjectMeta {
     labels?: { [key: string]: string };
     annotations?: { [key: string]: string };
     resourceVersion?: string;
+    finalizers?: string[];
+    deletionTimestamp?: string;
+    deletionGracePeriodSeconds?: number;
 }

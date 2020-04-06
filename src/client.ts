33ee0cbe549fb32466bfbef4a242d122df778565
Martin Helmich <m.helmich@mittwald.de>
2020-04-06 09:52:51 +0200

    Update Jest and other test libraries

--- src/client.ts
+++ src/client.ts
@@ -1,4 +1,4 @@
-import * as request from "request";
+import request from "request";
 import {IKubernetesClientConfig} from "./config";
 import {Selector, selectorToQueryString} from "./label";
 import {isStatus, MetadataObject} from "./types/meta";

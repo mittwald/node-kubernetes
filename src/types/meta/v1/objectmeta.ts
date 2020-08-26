d19a994303f5c75bb37bf75c1be4fd3924d3c9f1
Martin Helmich <m.helmich@mittwald.de>
2020-08-26 09:30:10 +0200

    Add "generateName" field to ObjectMeta

--- src/types/meta/v1/objectmeta.ts
+++ src/types/meta/v1/objectmeta.ts
@@ -1,6 +1,7 @@
 export interface ObjectMeta {
     name: string;
     namespace?: string;
+    generateName?: string;
     labels?: { [key: string]: string };
     annotations?: { [key: string]: string };
     resourceVersion?: string;

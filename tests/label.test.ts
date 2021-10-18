26259e4d954bb22c72947ec6f4954411880dddfa
Ruven Fehling <r.fehling@mittwald.de>
2021-10-18 15:58:15 +0200

    Add tests

--- tests/label.test.ts
+++ tests/label.test.ts
@@ -9,4 +9,26 @@ describe("Label selector", () => {
         const qs = selectorToQueryString({foo: "bar", bar: "baz"});
         expect(qs).toEqual("foo=bar,bar=baz");
     });
+    test("should convert match expression to correct query parameter", () => {
+        const qs = selectorToQueryString({
+            scope: {
+                operator: "in",
+                values: ["internal", "external"]
+            }
+        });
+        expect(qs).toEqual("scope in (internal,external)");
+    });
+    test("should convert match expression with multiple lables", () => {
+        const qs = selectorToQueryString({
+            scope: {
+                operator: "in",
+                values: ["internal", "external"]
+            },
+            foo: {
+                operator: "notin",
+                values: ["bar", "cafe"]
+            }
+        });
+        expect(qs).toEqual("scope in (internal,external),foo notin (bar,cafe)");
+    });
 });

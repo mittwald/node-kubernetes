7b5e9ccba44b2ec71de8de23a0ee5f2390d638c7
fffunke <50409802+fffunke@users.noreply.github.com>
2023-05-02 15:08:44 +0200

    add conversionReview types (#71)
    * add conversionReview types
    
    * update index.ts

---
+++ src/types/apiextensions/v1/conversionreview.ts
@@ -0,0 +1,29 @@
+import {Resource, Status} from "../../meta";
+
+export declare const apiGroup = "apiextensions.k8s.io/v1";
+
+export declare const conversionReviewKind = "ConversionReview";
+export declare type ConversionReview<TRequest, TResponse> = Resource<
+        typeof conversionReviewKind,
+        typeof apiGroup
+> &
+        (
+                | {
+            request: ConversionRequest<TRequest>;
+        }
+                | {
+            response: ConversionResponse<TResponse>;
+        }
+                );
+
+export interface ConversionRequest<TObject> {
+    uid: string;
+    desiredAPIVersion: string;
+    objects: TObject[];
+}
+
+export interface ConversionResponse<TObject> {
+    uid: string;
+    convertedObjects?: TObject[];
+    result: Status;
+}

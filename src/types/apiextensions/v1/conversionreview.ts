d0e4115680179a9cd3785b990533b5ffd328b93b
fffunke <50409802+fffunke@users.noreply.github.com>
2023-06-01 09:53:09 +0200

    use ConversionResponseStatus (#78)

--- src/types/apiextensions/v1/conversionreview.ts
+++ src/types/apiextensions/v1/conversionreview.ts
@@ -1,6 +1,11 @@
-import { Resource, Status } from "../../meta";
+import { Resource } from "../../meta";
 
 export declare const apiGroup = "apiextensions.k8s.io/v1";
+
+export declare type ConversionResponseStatus = {
+    status: "Success" | "Failed";
+    message?: string;
+};
 
 export declare const conversionReviewKind = "ConversionReview";
 export declare type ConversionReview<TRequest, TResponse> = Resource<typeof conversionReviewKind, typeof apiGroup> &
@@ -22,5 +27,5 @@ export interface ConversionRequest<TObject> {
 export interface ConversionResponse<TObject> {
     uid: string;
     convertedObjects?: TObject[];
-    result: Status;
+    result: ConversionResponseStatus;
 }
